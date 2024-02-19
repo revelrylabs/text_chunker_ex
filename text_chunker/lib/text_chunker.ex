f9c88b7d2f52b57559c50cb033fdb9a6cebf58bb
Stuart Page <38261603+stuartjohnpage@users.noreply.github.com>
2024-02-19 11:57:47 -0600

    initial commit

---
+++ text_chunker/lib/text_chunker.ex
@@ -0,0 +1,18 @@
+defmodule TextChunker do
+  @moduledoc """
+  Documentation for `TextChunker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TextChunker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ text_chunker/test/text_chunker_test.exs
@@ -0,0 +1,8 @@
+defmodule TextChunkerTest do
+  use ExUnit.Case
+  doctest TextChunker
+
+  test "greets the world" do
+    assert TextChunker.hello() == :world
+  end
+end
