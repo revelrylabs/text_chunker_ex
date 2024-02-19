6e139b4c9e6351a07f96ef11461e3d09e10bf582
Stuart Page <38261603+stuartjohnpage@users.noreply.github.com>
2024-02-19 14:45:28 -0600

    adds initial implementation and tests

---
+++ lib/chunker/recursive_split.ex
@@ -0,0 +1,157 @@
+defmodule Chunker.RecursiveSplit do
+  @moduledoc """
+  Handles recursion-based text splitting into chunks, adhering to configured size
+  and overlap requirements. It effectively divides text using various delimiters,
+  ensuring logical consistency across chunks by preserving context where needed.
+  """
+  def recursive_split(text, separators, chunk_size, chunk_overlap) do
+    {current_separator, remaining_separators} = get_active_separator(separators, text)
+
+    {final_chunks, good_splits} =
+      current_separator
+      |> split_on_separator(text)
+      |> Enum.reduce({[], []}, fn chunk, {final_chunks, good_splits} ->
+        cond do
+          chunk_small_enough?(chunk, chunk_size) ->
+            {final_chunks, good_splits ++ [chunk]}
+
+          Enum.empty?(remaining_separators) ->
+            final_chunks =
+              merge_good_splits_into_final_chunks(
+                good_splits,
+                final_chunks,
+                chunk_size,
+                chunk_overlap
+              )
+
+            {final_chunks ++ [chunk], []}
+
+          true ->
+            final_chunks =
+              merge_good_splits_into_final_chunks(
+                good_splits,
+                final_chunks,
+                chunk_size,
+                chunk_overlap
+              )
+
+            more_chunks = recursive_split(chunk, remaining_separators, chunk_size, chunk_overlap)
+            {final_chunks ++ more_chunks, []}
+        end
+      end)
+
+    final_chunks =
+      merge_good_splits_into_final_chunks(
+        good_splits,
+        final_chunks,
+        chunk_size,
+        chunk_overlap
+      )
+
+    final_chunks
+  end
+
+  defp merge_good_splits_into_final_chunks(good_splits, final_chunks, chunk_size, chunk_overlap, separator \\ "") do
+    case good_splits do
+      [] -> final_chunks
+      _ -> final_chunks ++ merge_splits(good_splits, chunk_size, chunk_overlap, separator)
+    end
+  end
+
+  # Fallback to an empty string as separator. This means it's one gigantic line of nothingness
+  # with no separators. It will get ignored inside create_splitument_segments. The separator we
+  # pass back is meaningless (unless it's "" in which case we will get segments
+  # but it might be very slow.)
+  defp get_active_separator([], _text) do
+    {" ", []}
+  end
+
+  # Gets the separator to be used on this round of iteration. For example:
+  # The list ["\n\n", "\n" ," "].
+  # If the text has "\n\n", then that will be the separator, and it drops that from the list
+  # If the text does't have it, then it goes to "\n" and so forth
+  defp get_active_separator(all_separators, text) do
+    [active_separator | rest] = all_separators
+
+    if String.contains?(text, active_separator) do
+      {active_separator, rest}
+    else
+      get_active_separator(rest, text)
+    end
+  end
+
+  defp chunk_small_enough?(chunk, max_chunk_size), do: String.length(chunk) <= max_chunk_size
+
+  # Collapses the splits based on separators into the correct chunk_size, and adds the overlap
+  defp merge_splits(splits, chunk_size, chunk_overlap, current_separator) do
+    {final_splits, current_splits, _split_total_length} =
+      Enum.reduce(splits, {[], [], 0}, fn split, {final_splits, current_splits, splits_total_length} ->
+        split_length = String.length(split)
+
+        bigger_than_chunk? =
+          splits_bigger_than_chunk?(
+            split_length,
+            current_splits,
+            splits_total_length,
+            current_separator,
+            chunk_size
+          )
+
+        if bigger_than_chunk? and !Enum.empty?(current_splits) do
+          final_splits_to_add = join_splits(current_splits, current_separator)
+
+          {splits_total_length, current_splits} =
+            reduce_chunk_size(
+              splits_total_length,
+              chunk_overlap,
+              chunk_size,
+              split_length,
+              current_splits
+            )
+
+          {final_splits ++ [final_splits_to_add], current_splits ++ [split], splits_total_length + split_length}
+        else
+          {final_splits, current_splits ++ [split], splits_total_length + split_length}
+        end
+      end)
+
+    leftover_splits = join_splits(current_splits, current_separator)
+    final_splits ++ [leftover_splits]
+  end
+
+  # Checks if the combined splits is bigger than the chunk
+  defp splits_bigger_than_chunk?(length, current_splits, splits_total_length, separator, chunk_size) do
+    additional_length = if Enum.count(current_splits) > 0, do: String.length(separator), else: 0
+    length + splits_total_length + additional_length > chunk_size
+  end
+
+  # Using the given separator, joins the strings in the array current_splits together
+  defp join_splits(current_splits, separator) do
+    result = Enum.join(current_splits, separator)
+    if String.equivalent?(result, ""), do: nil, else: result
+  end
+
+  # Recursively reduces the chunk size. The function operates when either
+  # a) the current total length of the splits exceeds the chunk overlap - this is where we create the chunk overlap
+  # b) when the sum of the combined splits's total length and the length of the current split exceeds the chunk size.
+  defp reduce_chunk_size(splits_total_length, chunk_overlap, chunk_size, split_length, current_splits)
+       when splits_total_length > chunk_overlap or (splits_total_length + split_length > chunk_size and splits_total_length > 0) do
+    new_total = splits_total_length - String.length(Enum.at(current_splits, 0))
+    [_head | rest] = current_splits
+    reduce_chunk_size(new_total, chunk_overlap, chunk_size, split_length, rest)
+  end
+
+  # Recursive base case, only used when the function above doesn't operate - starts off the next split with the overlap from the previous split
+  defp reduce_chunk_size(splits_total_length, _chunk_overlap, _chunk_size, _split_length, current_splits) do
+    {splits_total_length, current_splits}
+  end
+
+  defp split_on_separator(separator, text) do
+    escaped_separator = escape_special_chars(separator)
+    Regex.split(~r/(?=#{escaped_separator})/u, text, trim: true)
+  end
+
+  defp escape_special_chars(separator) do
+    Regex.replace(~r/([\/\-\\\^\$\*\+\?\.\(\)\|\[\]\{\}])/u, separator, "\\\\\\0")
+  end
+end
---
+++ test/recursive_split_test.exs
@@ -0,0 +1,162 @@
+defmodule RecursiveSplitTest do
+  alias Chunker.RecursiveSplit
+
+  use ExUnit.Case
+
+  @moduletag timeout: :infinity
+
+  describe "plaintext splitter" do
+    test "splits multiple sentences correctly" do
+      chunk_size = 50
+      chunk_overlap = 10
+
+      text =
+        "This is quite a short sentence. But what a headache does the darn thing create! Especially when splitting is involved. Do not look for meaning."
+
+      separators = Separators.get_separators(:plaintext)
+      result = RecursiveSplit.recursive_split(text, separators, chunk_size, chunk_overlap)
+
+      expected_result = [
+        "This is quite a short sentence. But what a",
+        " what a headache does the darn thing create!",
+        " create! Especially when splitting is involved. Do",
+        " Do not look for meaning."
+      ]
+
+      assert result == expected_result
+    end
+
+    test "splits a piece of short text correctly" do
+      chunk_size = 10
+      chunk_overlap = 2
+      text = "Hello there!\n General sdKenobi..."
+      separators = Separators.get_separators(:plaintext)
+      result = RecursiveSplit.recursive_split(text, separators, chunk_size, chunk_overlap)
+      expected_result = ["Hello", " there!", "\n General", " sdKenobi.", "i..."]
+
+      assert result == expected_result
+    end
+
+    test "splits a longer text correctly" do
+      chunk_size = 10
+      chunk_overlap = 2
+
+      text =
+        "This is a text splitter.\nIt splits text.\n\nThis is a completely separate paragraph of context."
+
+      separators = Separators.get_separators(:plaintext)
+      result = RecursiveSplit.recursive_split(text, separators, chunk_size, chunk_overlap)
+
+      expected_result = [
+        "This is a",
+        " a text",
+        " splitter.",
+        "\nIt splits",
+        " text.",
+        "\n",
+        "\nThis is a",
+        " completel",
+        "ely",
+        " separate",
+        " paragraph",
+        " of",
+        " context."
+      ]
+
+      assert result == expected_result
+    end
+
+    test "copies the test from langchain" do
+      chunk_size = 10
+      chunk_overlap = 1
+      text = "Hi.\n\nI'm Harrison.\n\nHow? Are? You?\nOkay then f f f f.
+      This is a weird text to write, but gotta test the splittingggg some how.\n\n
+      Bye!\n\n-H."
+      separators = Separators.get_separators(:plaintext)
+      result = RecursiveSplit.recursive_split(text, separators, chunk_size, chunk_overlap)
+
+      expected_result = [
+        "Hi.",
+        "\n",
+        "\nI'm",
+        " Harrison.",
+        "\n",
+        "\nHow? Are?",
+        " You?",
+        "\nOkay then",
+        " f f f f.",
+        "\n     ",
+        "  This is",
+        " a weird",
+        " text to",
+        " write,",
+        " but gotta",
+        " test the",
+        " splitting",
+        "gggg",
+        " some how.",
+        "\n",
+        "\n",
+        "\n     ",
+        "  Bye!",
+        "\n\n-H."
+      ]
+
+      assert result == expected_result
+    end
+
+    test "splits a huge file correctly" do
+      chunk_size = 1000
+      chunk_overlap = 200
+      {:ok, text} = File.read("test/support/fixtures/document_fixtures/hamlet.txt")
+      separators = Separators.get_separators(:plaintext)
+
+      result =
+        text
+        |> RecursiveSplit.recursive_split(separators, chunk_size, chunk_overlap)
+        |> Enum.take(2)
+
+      expected_result = first_split_hamlet()
+
+      assert result == expected_result
+    end
+  end
+
+  describe "markdown splitter" do
+    test "splits a simple markdown file" do
+      chunk_size = 100
+      chunk_overlap = 20
+      {:ok, text} = File.read("test/support/fixtures/document_fixtures/test_file.md")
+
+      separators = Separators.get_separators(:markdown)
+
+      result = RecursiveSplit.recursive_split(text, separators, chunk_size, chunk_overlap)
+
+      expected_result = [
+        "# Foobar\n\nFoobar is a Python library for dealing with word pluralization.\n",
+        "\n## Installation\n\nUse the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.",
+        "\n\n```bash\npip install foobar\n```\n",
+        "\n## Usage\n\n```python\nimport foobar\n\n# returns 'words'\nfoobar.pluralize('word')",
+        "\n\n# returns 'geese'\nfoobar.pluralize('goose')",
+        "\n\n# returns 'phenomenon'\nfoobar.singularize('phenomena')\n```\n",
+        "\n## Contributing",
+        "\n\nPull requests are welcome. For major changes, please open an issue first",
+        "\nto discuss what you would like to change.",
+        "\n\nPlease make sure to update tests as appropriate.\n",
+        "\n## License\n\n[MIT](https://choosealicense.com/licenses/mit/)"
+      ]
+
+      assert result == expected_result
+    end
+  end
+
+    @doc """
+  move this
+  """
+  def first_split_hamlet do
+    [
+      "THE TRAGEDY OF HAMLET, PRINCE OF DENMARK\n\n\nby William Shakespeare\n\n\n\nDramatis Personae\n\n  Claudius, King of Denmark.\n  Marcellus, Officer.\n  Hamlet, son to the former, and nephew to the present king.\n  Polonius, Lord Chamberlain.\n  Horatio, friend to Hamlet.\n  Laertes, son to Polonius.\n  Voltemand, courtier.\n  Cornelius, courtier.\n  Rosencrantz, courtier.\n  Guildenstern, courtier.\n  Osric, courtier.\n  A Gentleman, courtier.\n  A Priest.\n  Marcellus, officer.\n  Bernardo, officer.\n  Francisco, a soldier\n  Reynaldo, servant to Polonius.\n  Players.\n  Two Clowns, gravediggers.\n  Fortinbras, Prince of Norway.  \n  A Norwegian Captain.\n  English Ambassadors.\n\n  Getrude, Queen of Denmark, mother to Hamlet.\n  Ophelia, daughter to Polonius.\n\n  Ghost of Hamlet's Father.\n\n  Lords, ladies, Officers, Soldiers, Sailors, Messengers, Attendants.\n\n\n\n\n\nSCENE.- Elsinore.\n\n\nACT I. Scene I.\nElsinore. A platform before the Castle.",
+      "\n\n  Ghost of Hamlet's Father.\n\n  Lords, ladies, Officers, Soldiers, Sailors, Messengers, Attendants.\n\n\n\n\n\nSCENE.- Elsinore.\n\n\nACT I. Scene I.\nElsinore. A platform before the Castle.\n\nEnter two Sentinels-[first,] Francisco, [who paces up and down\nat his post; then] Bernardo, [who approaches him].\n\n  Ber. Who's there.?\n  Fran. Nay, answer me. Stand and unfold yourself.\n  Ber. Long live the King!\n  Fran. Bernardo?\n  Ber. He.\n  Fran. You come most carefully upon your hour.\n  Ber. 'Tis now struck twelve. Get thee to bed, Francisco.\n  Fran. For this relief much thanks. 'Tis bitter cold,\n    And I am sick at heart.\n  Ber. Have you had quiet guard?\n  Fran. Not a mouse stirring.\n  Ber. Well, good night.\n    If you do meet Horatio and Marcellus,\n    The rivals of my watch, bid them make haste.\n\n                    Enter Horatio and Marcellus.  "
+    ]
+  end
+end
