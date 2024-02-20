5737281679f6a68e98a6ff157489d0f8699b2154
Stuart Page <38261603+stuartjohnpage@users.noreply.github.com>
2024-02-20 10:38:19 -0600

    cleans up test

--- test/recursive_split_test.exs
+++ test/recursive_split_test.exs
@@ -124,7 +124,6 @@ defmodule RecursiveSplitTest do
       ]
 
       {:ok, text} = File.read("test/support/fixtures/document_fixtures/hamlet.txt")
-      separators = Separators.get_separators(:plaintext)
 
       result =
         text
@@ -167,10 +166,7 @@ defmodule RecursiveSplitTest do
     end
   end
 
-  @doc """
-  move this
-  """
-  def first_split_hamlet do
+  defp first_split_hamlet do
     [
       "THE TRAGEDY OF HAMLET, PRINCE OF DENMARK\n\n\nby William Shakespeare\n\n\n\nDramatis Personae\n\n  Claudius, King of Denmark.\n  Marcellus, Officer.\n  Hamlet, son to the former, and nephew to the present king.\n  Polonius, Lord Chamberlain.\n  Horatio, friend to Hamlet.\n  Laertes, son to Polonius.\n  Voltemand, courtier.\n  Cornelius, courtier.\n  Rosencrantz, courtier.\n  Guildenstern, courtier.\n  Osric, courtier.\n  A Gentleman, courtier.\n  A Priest.\n  Marcellus, officer.\n  Bernardo, officer.\n  Francisco, a soldier\n  Reynaldo, servant to Polonius.\n  Players.\n  Two Clowns, gravediggers.\n  Fortinbras, Prince of Norway.  \n  A Norwegian Captain.\n  English Ambassadors.\n\n  Getrude, Queen of Denmark, mother to Hamlet.\n  Ophelia, daughter to Polonius.\n\n  Ghost of Hamlet's Father.\n\n  Lords, ladies, Officers, Soldiers, Sailors, Messengers, Attendants.\n\n\n\n\n\nSCENE.- Elsinore.\n\n\nACT I. Scene I.\nElsinore. A platform before the Castle.",
       "\n\n  Ghost of Hamlet's Father.\n\n  Lords, ladies, Officers, Soldiers, Sailors, Messengers, Attendants.\n\n\n\n\n\nSCENE.- Elsinore.\n\n\nACT I. Scene I.\nElsinore. A platform before the Castle.\n\nEnter two Sentinels-[first,] Francisco, [who paces up and down\nat his post; then] Bernardo, [who approaches him].\n\n  Ber. Who's there.?\n  Fran. Nay, answer me. Stand and unfold yourself.\n  Ber. Long live the King!\n  Fran. Bernardo?\n  Ber. He.\n  Fran. You come most carefully upon your hour.\n  Ber. 'Tis now struck twelve. Get thee to bed, Francisco.\n  Fran. For this relief much thanks. 'Tis bitter cold,\n    And I am sick at heart.\n  Ber. Have you had quiet guard?\n  Fran. Not a mouse stirring.\n  Ber. Well, good night.\n    If you do meet Horatio and Marcellus,\n    The rivals of my watch, bid them make haste.\n\n                    Enter Horatio and Marcellus.  "
