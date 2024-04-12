681f6c9c180b468cd30be7fc5838f70b8747f07c
Stuart Page <38261603+stuartjohnpage@users.noreply.github.com>
2024-04-11 23:12:29 -0500

    removes tuple requirement

--- lib/text_chunker.ex
+++ lib/text_chunker.ex
@@ -11,7 +11,7 @@ defmodule TextChunker do
   **Supported Options**
   * `:chunk_size` (positive integer, default: 2000) - Maximum size in code point length for each chunk.
   * `:chunk_overlap` (non-negative integer, default: 200) - Number of overlapping code points between consecutive chunks to preserve context.
-  * `:strategy` (module tuple, default: `{RecursiveChunk, nil}`) - A module implementing the split function. Currently only `RecursiveChunk` is supported.
+  * `:strategy` (module default: `RecursiveChunk`) - A module implementing the split function. Currently only `RecursiveChunk` is supported.
   * `:format` (atom, default: `:plaintext`) - The format of the input text. Used to determine where to split the text in some strategies.
   """
   alias TextChunker.Strategies.RecursiveChunk
