9b5f20a0b634c0945b80054c519b631bbd1dd222
Stuart Page <38261603+stuartjohnpage@users.noreply.github.com>
2026-08-24 13:50:04 -0500

    removed comment

--- .credo.exs
+++ .credo.exs
@@ -4,7 +4,6 @@
       name: "default",
       strict: true,
       checks: [
-        # The recursive chunk assembly loops legitimately nest to depth 3.
         {Credo.Check.Refactor.Nesting, max_nesting: 3}
       ]
     }
